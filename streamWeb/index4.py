39f1d9172109787e55418da768eeebb25b4183ec
廖奕豪 <106504824+alan7651@users.noreply.github.com>
2024-06-15 05:32:01 +0000

    modify

--- streamWeb/index4.py
+++ streamWeb/index4.py
@@ -4,11 +4,13 @@ import time
 placeholder = st.empty()
 
 with placeholder:
+    total_sec = 0
     for each_second in range(5):
         st.write(f"{each_second} seconds have passed")
         time.sleep(1)
+        total_sec += 1
 
-    st.write("🥳 1 minute over!")
+    st.write(f"🥳 {total_sec} seconds is over!")
 
 time.sleep(5)
 placeholder.empty()
