c4a6f81f064e86b8e871161d40367dec7adbaa27
廖奕豪 <106504824+alan7651@users.noreply.github.com>
2024-06-15 03:47:04 +0000

    add numpy randn barchart

--- streamWeb/index1.py
+++ streamWeb/index1.py
@@ -16,4 +16,9 @@ from requests import Response
 aqi_url = "https://data.moenv.gov.tw/api/v2/aqx_p_488?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON"
 res:Response = requests.get(aqi_url)
 
+if res.status_code == 200:
+    print("下載成功")
+else:
+    print("下載失敗")
+      
 ''')
