9ed721dd7117988462c5307ba1b8570218b5cd91
廖奕豪 <106504824+alan7651@users.noreply.github.com>
2024-06-15 03:59:35 +0000

    empty():countdown and clear

--- streamWeb/index4.py
+++ streamWeb/index4.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import time
 
-with st.empty():
-    for each_second in range(60):
+placeholder = st.empty()
+
+with placeholder:
+    for each_second in range(5):
         st.write(f"{each_second} seconds have passed")
         time.sleep(1)
 
     st.write("🥳 1 minute over!")
+
+time.sleep(5)
+placeholder.empty()
