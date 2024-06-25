72fa8c8e596ade30e4baac41d0c3670e2986e94c
alan7651 <106504824+alan7651@users.noreply.github.com>
2024-06-25 18:05:18 +0800

    modify without rootmodel

--- lesson13/index2.py
+++ lesson13/index2.py
@@ -25,8 +25,7 @@ class Site(BaseModel):
     def abcd(cls,value):
         return f'{value[:4]}-{value[4:6]}-{value[6:8]} {value[8:10]}:{value[10:12]}:{value[12:]}'
     
-    @field_serializer('狀態',when_used='always')
-    @classmethod
+    @field_serializer('狀態')
     def abce(self,value):
         if value:
             return "營業中"
@@ -34,28 +33,40 @@ class Site(BaseModel):
             return "維護中"
 
 # 將Site Class資料轉為 root 元件        
-class Root(RootModel):
+class Root(BaseModel):
     root:list[Site]
 
 
 @st.cache_data
-def download_youbike()->str:
+def download_youbike():
     youbike_url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/json?size=2000'
     try:
         response = requests.get(youbike_url)
     except Exception as e:
         print(e)
     else:
-        return response.text
+        return response.json()
 
 data_str = download_youbike()
-root = Root.model_validate_json(data_str)
-data = root.model_dump()
+#print(type(data_str))
+#data = Root(root=data_str)
+root =[]
+for each_site in data_str:
+    valid_data = Site.model_validate_json(json.dumps(each_site,ensure_ascii=False))
+    if valid_data.狀態 == True:
+        valid_data.狀態 = '營業中'
+    else:
+        valid_data.狀態 = '維護中'
+    root.append(valid_data)
+#data = root.model_dump()
+#print(root)
+# def ijk(value):
+#    return value['行政區']
 
-def ijk(value):
-    return value['行政區']
-
-areas:list[str] = list(set(map(ijk,data)))
+areas = []
+for item in root:
+    if item.行政區 not in areas:
+        areas.append(item.行政區)
 
 option = st.selectbox("請選擇行政區",areas)
 st.write("您選擇:", option)
