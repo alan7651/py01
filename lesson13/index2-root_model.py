fc46aac94b3afa51c5c4d7827ad11ed7dd73a659
alan7651 <106504824+alan7651@users.noreply.github.com>
2024-06-29 09:09:39 +0800

    modify

--- lesson13/index2-root_model.py
+++ lesson13/index2-root_model.py
@@ -1,7 +1,6 @@
 import requests
 from pydantic import BaseModel,Field,RootModel,field_validator,field_serializer
 import streamlit as st
-import json
 
 class Site(BaseModel):
     站點名稱:str = Field(alias="sna")
@@ -31,43 +30,30 @@ class Site(BaseModel):
             return "營業中"
         else:
             return "維護中"
-
-# 將Site Class資料轉為 root 元件        
-class Root(BaseModel):
+        
+class Root(RootModel):
     root:list[Site]
 
 
 @st.cache_data
-def download_youbike():
+def download_youbike()->str:
     youbike_url = 'https://data.ntpc.gov.tw/api/datasets/010e5b15-3823-4b20-b401-b1cf000550c5/json?size=20'
     try:
         response = requests.get(youbike_url)
     except Exception as e:
         print(e)
     else:
-        return response.json()
+        return response.text
 
 data_str = download_youbike()
-#print(type(data_str))
-#data = Root(root=data_str)
-root =[]
-for each_site in data_str:
-    valid_data = Site.model_validate_json(json.dumps(each_site,ensure_ascii=False))
-    if valid_data.狀態 == True:
-        valid_data.狀態 = '營業中'
-    else:
-        valid_data.狀態 = '維護中'
-    root.append(valid_data)
-#data = root.model_dump()
-#print(root)
-# def ijk(value):
-#    return value['行政區']
+root = Root.model_validate_json(data_str)
+data = root.model_dump()
 
-print(root)
-areas = []
-for item in root:
-    if item.行政區 not in areas:
-        areas.append(item.行政區)
+print(data)
+def ijk(value):
+    return value['行政區']
+
+areas:list[str] = list(set(map(ijk,data)))
 
 option = st.selectbox("請選擇行政區",areas)
 st.write("您選擇:", option)
